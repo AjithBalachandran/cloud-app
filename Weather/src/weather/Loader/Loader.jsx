04de7498fb009fd877635ce83b477b6ab4daf36f
AjithBalachandran <jthbl26@gmail.com>
2023-04-21 13:36:23 +0530

    fianl

---
+++ Weather/src/weather/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Loader.css"
+
+const Loader = () => {
+    return (
+        <div>
+            <h1>Enter the city name</h1>
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+export default Loader;
--- Weather/src/weather/Weather.jsx
+++ Weather/src/weather/Weather.jsx
@@ -3,24 +3,33 @@ import "./Weather.css";
 import { FiSearch } from "react-icons/fi";
 import axios from "axios";
 import { useQuery } from "react-query";
+import Loader from "./Loader/Loader";
 
 const Weather = () => {
-  const [place, setPalce] = useState("London");
+  const [place, setPalce] = useState("");
 
   //function for fetch data from the api
   const fetchData = () => {
-    return axios.get(`http://localhost:4000/:${place}`);
+     return axios.get(`http://localhost:4000/:${place}`);
+   
   };
   // use useQuery for fetch the data
   const { data, refetch } = useQuery("weather", fetchData, { enabled: false });
-  console.log(data);
+  // onClick funtion
+  const handleData = (place) =>{
+    if(place === ""){
+      console.log("invalid city name");
+    }else{
+      refetch();
+    }
+  }
 
-  console.log(data);
+ 
   //using ref for get input box for focus
   const inputRef = useRef();
-
   //for focus the input box in initial load
   useEffect(() => {
+    inputRef.current.focus();
     fetchData();
   }, []);
   return (
@@ -34,14 +43,13 @@ const Weather = () => {
             ref={inputRef}
             onChange={(e) => setPalce(e.target.value)}
             required
-          />{" "}
-          <span className="validator">This field is rquired</span>
-          <button onClick={refetch}>
+          />
+          <button onClick={()=>handleData(place)}>
             <FiSearch />
           </button>
         </div>
 
-        <div className="weather-details">
+       {data ? <div className="weather-details">
           <img
             src={data?.data?.current.condition?.icon}
             alt=""
@@ -53,8 +61,9 @@ const Weather = () => {
           </h1>
           <h2>{data?.data?.location.name}</h2>
           <h2>{data?.data?.current.condition?.text}</h2>
-        </div>
-        <div className="details">
+        </div> :<Loader />}
+        
+       {data ? <div className="details">
           <div className="other-details">
             <h1>
               <u>Other Details</u>
@@ -72,8 +81,9 @@ const Weather = () => {
           <h3>
             Time Zone : <span>{data?.data?.location?.tz_id} </span>
           </h3>
-        </div>
+        </div> : ""}
       </div>
+      {place === "invalid" ? <h1>Invalid city</h1>: ""}
     </div>
   );
 };
